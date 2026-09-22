9886494f8b58d9cc153b18a594267865177e0539
jzy834894319 <52372061+jzy834894319@users.noreply.github.com>
2026-09-22 14:39:40 +0800

    feat(plugin): add direct and playlist chapter modes

--- lib/plugins/api_rule_config.dart
+++ lib/plugins/api_rule_config.dart
@@ -1,9 +1,16 @@
 class RuleMode {
   static const String xpath = 'xpath';
   static const String api = 'api';
+  static const String direct = 'direct';
+  static const String playlist = 'playlist';
 
   static String normalize(Object? value) {
-    return value == api ? api : xpath;
+    return switch (value) {
+      api => api,
+      direct => direct,
+      playlist => playlist,
+      _ => xpath,
+    };
   }
 }
 
